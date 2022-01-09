d23f76c55f6b77ba5ee452614b5ff0df01d0e447
muhammad fadhil pratama <fadhilpratamaC3@gmail.com>
2022-01-09 18:38:01 +0700

    penyelesaian

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="/img/Fadhil.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Halaman Admin</span>
+      <span class="brand-text font-weight-light">Halaman Pemain</span>
     </a>
     <!-- Sidebar -->
     <div class="sidebar">
